d66b5b6c88ddcc6dea30c5b38a405908530360d5
gaojuqian <jqiown@163.com>
2022-07-23 23:11:37 +0800

    day1

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@ use std::io;
 
 fn main() {
     let rand_num = rand::thread_rng().gen_range(1..=100);
-    let mut input_num: String = String::new();
 
-    println!("请输入 -> {}", rand_num);
+    loop {
+        let mut input_num: String = String::new();
+        println!("请输入 -> {rand_num}-{input_num}");
 
-    io::stdin().read_line(&mut input_num).expect("用户输入");
+        io::stdin().read_line(&mut input_num).expect("用户输入");
 
-    let input_num :u32 = input_num.trim().parse().expect("输入数字");
+        let input_num: u32 = input_num.trim().parse().expect("err");
 
-    match input_num.cmp(&rand_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match input_num.cmp(&rand_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
