5ee695bd9f44b4977278ee5ccbc2d9e48d44b9ad
gaojuqian <jqiown@163.com>
2022-07-31 21:29:43 +0800

    Day2

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,46 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+// use rand::Rng;
+// use std::cmp::Ordering;
+// use std::io;
+
+const CONST_PI:f32 = 3.14;
 
 fn main() {
-    let rand_num = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        let mut input_num: String = String::new();
-        println!("请输入 -> {rand_num}-{input_num}");
-
-        io::stdin().read_line(&mut input_num).expect("用户输入");
-
-        let input_num: u32 = match input_num.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        match input_num.cmp(&rand_num) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    day2()
 }
+
+fn day2() {
+    let a = 1;
+    println!("{a}{CONST_PI}");
+    let mut a = 2;
+    println!("{a}");
+    a = 123;
+    println!("{a}");
+}
+
+// fn day1() {
+//     let rand_num = rand::thread_rng().gen_range(1..=100);
+
+//     loop {
+//         let mut input_num: String = String::new();
+//         println!("请输入 -> {rand_num}-{input_num}");
+
+//         match io::stdin().read_line(&mut input_num) {
+//             Ok(val) => val,
+//             Err(_) => continue,
+//         };
+
+//         let input_num: u32 = match input_num.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue,
+//         };
+
+//         match input_num.cmp(&rand_num) {
+//             Ordering::Less => println!("Too small!"),
+//             Ordering::Greater => println!("Too big!"),
+//             Ordering::Equal => {
+//                 println!("You win!");
+//                 break;
+//             }
+//         }
+//     }
+// }
