afe1dc11d964b6b12ebb3d6c0defe249ee1ba788
gaojuqian <jqiown@163.com>
2022-08-06 21:29:11 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,17 @@ fn day2() {
 
 // fn day1() {
 //     let rand_num = rand::thread_rng().gen_range(1..=100);
-
 //     loop {
 //         let mut input_num: String = String::new();
 //         println!("请输入 -> {rand_num}-{input_num}");
-
 //         match io::stdin().read_line(&mut input_num) {
 //             Ok(val) => val,
 //             Err(_) => continue,
 //         };
-
 //         let input_num: u32 = match input_num.trim().parse() {
 //             Ok(num) => num,
 //             Err(_) => continue,
 //         };
-
 //         match input_num.cmp(&rand_num) {
 //             Ordering::Less => println!("Too small!"),
 //             Ordering::Greater => println!("Too big!"),
