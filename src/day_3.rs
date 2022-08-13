f8976ed41c2a32431b25ee235343498250d3cca6
gaojuqian <jqiown@163.com>
2022-08-13 22:52:03 +0800

    5

--- src/day_3.rs
+++ src/day_3.rs
@@ -2,7 +2,7 @@ pub fn test(){
     let  _a = String::from("abc");
     // 存储堆 -> 移动
     // let _b = _a; 不可访问
-    
+
     // 克隆
     let  _b = _a.clone() + "456";
     println!("{}", format!("{_a},{_b}"));
@@ -12,4 +12,23 @@ pub fn test(){
     let _d = _c;
     println!("{}", format!("{_c:#?}"));
 
+
+    // 传递引用
+    let _e = String::from("hello, string");
+    let len = get_length(&_e);
+    println!("{_e}, {len}");
+
+    // 修改
+    let mut _f = String::from("hello");
+    edit_str(&mut _f);
+}
+
+
+fn get_length(item: &String) -> usize{
+    item.len()
+}
+
+fn edit_str(item: &mut String) -> usize{
+    item.push_str(", push");
+    item.len()
 }
